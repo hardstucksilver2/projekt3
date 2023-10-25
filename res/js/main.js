a14dd3fe6e5affd0c954fdbd92a7b6579e01b345
Silhyjelopata <matejsilhan12@seznam.cz>
2023-10-25 09:51:16 +0200

    f

---
+++ res/js/main.js
@@ -0,0 +1,41 @@
+
+/*const kuulin = (item1, item2) => {
+
+    console.log("Prinesl jsem " +item1+ " a "+item2);
+
+}
+
+//                   parametr
+function kulin(item){
+console.log("Prinesl jsem " + item);
+
+}
+
+
+
+
+kulin("michala")
+kuulin("mateje","mys")*/
+const sub= (a,b) => {
+
+    console.log(a - b);
+
+
+}
+
+sub(7,8)
+
+function sum(a, b, c, d, e, f){
+
+    console.log (a + b + c + d + e + f)
+}
+
+sum(1, 2, 3, 4, 5, 6)
+
+const greetings= (jmeno1,jmeno2, jmeno3, jmeno4) => {
+
+    console.log("Hello " + jmeno1 + ", Hello " + jmeno2+ ", Hello " + jmeno3 + ", Hello " +jmeno4);
+    console.log(`Hello ${jmeno1}, hello ${jmeno2}, hello ${jmeno3}, hello ${jmeno4}`)
+
+}
+greetings("paja" , "Matej" , "Misa " , "matej")
