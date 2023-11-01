775511059e4afc240606c06d3c9e54c13c5e6916
Silhyjelopata <matejsilhan12@seznam.cz>
2023-11-01 10:18:23 +0100

    k

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,14 @@
 const startButton = document.getElementById("startButton");
+const timeInfo = document.getElementById("timeInfo");
 const point = document.getElementById("point");
 let gameInterval;
+let timeStart;
 
 startButton.onclick = () => {
-  
   hideElement(startButton);
   startGameInterval(point);
+  setPointClick(point);
+  timeStart = performance.now();
 };
 const movePoint = (element, x, y) => {
   element.style.top = `${y}px`;
@@ -18,9 +21,39 @@ const hideElement = (element) => {
   element.style.display = "none";
 };
 
-const startGameInterval = () => {
+const startGameInterval = (element) => {
   clearInterval(gameInterval);
   gameInterval = setInterval(() => {
-    movePoint(point, getRandomNumber(50, 400), getRandomNumber(50, 400));
+    
+    setSize(element, getRandomNumber(20, 80));
+    movePoint(
+      element,
+      getRandomNumber(50, window.innerWidth - parseInt(element.style.width)),
+      getRandomNumber(50, window.innerHeight - parseInt(element.style.height))
+    );
+    
   }, 550);
+};
+
+const setPointClick = (element) => {
+  element.onclick = () => {
+    element.innerText++;
+
+    setSize(element, getRandomNumber(20, 80));
+    movePoint(
+      element,
+      getRandomNumber(50, window.innerWidth - parseInt(element.style.width)),
+      getRandomNumber(50, window.innerHeight - parseInt(element.style.height))
+    );
+   
+    let timeEnd = performance.now();
+    let result = timeEnd - timeStart;
+    timeInfo.innerText = `Time ${result}ms`;
+    timeStart = performance.now()
+  };
+};
+
+const setSize = (element, size) => {
+  element.style.width = ` ${size}px`;
+  element.style.height = ` ${size}px`;
 };
